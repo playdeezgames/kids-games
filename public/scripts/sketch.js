eb13ad0705884af8bcb2e8a492bff6bf59c2e8cd
Ernest Pazera <ernestpazera@gmail.com>
2020-03-12 18:36:54 +0000

    Update public/scripts/sketch.js

--- public/scripts/sketch.js
+++ public/scripts/sketch.js
@@ -1,8 +1,11 @@
 function setup() {
   var cnv = createCanvas(windowWidth, windowHeight);
-  cnv.style('display', 'block');
   background(255, 0, 200);
 }
 
 function draw() {
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
