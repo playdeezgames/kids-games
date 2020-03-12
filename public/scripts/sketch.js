020ff403bb7242b964d19918aeed63e6a5fc55a1
Ernest Pazera <ernestpazera@gmail.com>
2020-03-12 23:24:40 +0000

    Update public/scripts/sketch.js

--- public/scripts/sketch.js
+++ public/scripts/sketch.js
@@ -1,13 +1,52 @@
-let img;
+let imgs = {};
+let tiles = [
+  "####################",
+  "#@.................#",
+  "#..................#",
+  "#..................#",
+  "#..................#",
+  "#..................#",
+  "#..................#",
+  "#..................#",
+  "#..................#",
+  "#..................#",
+  "#..................#",
+  "#..................#",
+  "#..................#",
+  "#..................#",
+  "#..................#",
+  "#..................#",
+  "#..................#",
+  "#..................#",
+  "#..................#",
+  "#..................#",
+  "#..................#",
+  "####################"
+];
+
 function preload(){
-  img = loadImage('assets/images/meeple.png');
+  imgs.meeple = loadImage('assets/images/meeple.png');
+  imgs.brick = loadImage('assets/images/brick-wall.png');
 }
 
 function setup() {
-  createCanvas(800, 600);
+  createCanvas(960, 640);
   background(192,192,192);
-  image(img,0,0);
 }
 
 function draw() {
+  clear();
+  for(let y in tiles){
+    let line = tiles[y];
+    for(let x = 0; x< line.length; ++x){
+      let px = x * 32;
+      let py = y * 32;
+      let c = line.charAt(x);
+      if(c=='#'){
+        image(imgs.brick,px,py);
+      }else if(c=='@'){
+        image(imgs.meeple,px,py);
+      }
+    }
+  }
 }
