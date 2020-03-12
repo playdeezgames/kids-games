0a4e77ebf3be8add9a1f34f602869da0bfabafe8
Ernest Pazera <ernestpazera@gmail.com>
2020-03-12 18:25:58 +0000

    Update public/scripts/sketch.js

--- public/scripts/sketch.js
+++ public/scripts/sketch.js
@@ -1,6 +1,12 @@
 function setup() {
+  noCanvas();
   let btn = createButton('Push Me!');
+  btn.mousePressed(buttonPress);
 }
 
 function draw() {
+}
+
+function buttonPress(){
+  window.open("www.google.com");
 }
