2bd167a55fbad58a28c149483b66a480ae435c97
ernestpazera <ernestpazera@gmail.com>
2020-03-14 13:33:45 -0500

    map exporting scripts

---
+++ exportmaps.sh
@@ -0,0 +1,5 @@
+tiled --export-map csv ./maps/start.tmx ./public/assets/maps/start.csv
+tiled --export-map csv ./maps/northHall.tmx ./public/assets/maps/northHall.csv
+tiled --export-map csv ./maps/eastHall.tmx ./public/assets/maps/eastHall.csv
+tiled --export-map csv ./maps/southHall.tmx ./public/assets/maps/southHall.csv
+tiled --export-map csv ./maps/westHall.tmx ./public/assets/maps/westHall.csv
