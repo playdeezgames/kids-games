d68705d7a5e4f5ea6ebfe4b167cdefc05b9f363e
ernestpazera <ernestpazera@gmail.com>
2020-05-20 05:44:29 -0500

    counting in a line

---
+++ public/learning/math/howmany/main.js
@@ -0,0 +1,74 @@
+let gameState = {};
+function giveAnswer(answer){
+    if(gameState.answerGiven){
+        return;
+    }
+    gameState.answerGiven=true;;
+    if(answer==gameState.currentProblem){
+        gameState.roundsCorrect++;
+        document.getElementById("result").innerHTML=`<img src="./confirmed.png"/>`;
+        setTimeout(()=>{
+            nextProblem();
+        },2000);
+    }else{
+        gameState.badGuesses++;
+        document.getElementById("result").innerHTML=`<img src="./cancel.png"/>`;
+        setTimeout(()=>{
+            document.getElementById("result").innerHTML="";
+            gameState.answerGiven=false;
+        },2000);
+    }
+}
+function showProblem(){
+    let content = "";
+    content+="<p>";
+    for(let index=0;index<gameState.currentProblem;++index){
+        content+=`<img src="${gameState.whichImage}"/>`
+    }
+    content+="</p>";
+    content+="<p>"
+    for(let index=gameState.minimum;index<=gameState.maximum;++index){
+        content+=`<button onclick="giveAnswer(${index})">${index}</button>`;
+    }
+    content+=`</p>`;
+    content+=`<p><span id="result"></span></p>`;
+    setContent(content);
+}
+function gameOver(){
+    let content="";
+    content+=`<p>Rounds: ${gameState.roundsCorrect}</p>`;
+    content+=`<p>Wrong Guesses: ${gameState.badGuesses}</p>`;
+    setContent(content);
+}
+function nextProblem(){
+    if(gameState.roundsLeft<=0){
+        gameOver();
+        return;
+    }
+    let newProblem;
+    do{
+        newProblem = Math.floor(Math.random()*(gameState.maximum-gameState.minimum+1))+gameState.minimum;
+    }while(newProblem==gameState.currentProblem);
+    gameState.currentProblem = newProblem;
+    gameState.answerGiven = false;
+    gameState.roundsLeft--;
+    showProblem();
+}
+function startGame(whichImage){
+    gameState.whichImage = whichImage;
+    gameState.maximum = 20;
+    gameState.minimum = 1;
+    gameState.roundsLeft = 20;
+    gameState.roundsCorrect = 0;
+    gameState.badGuesses = 0;
+    gameState.currentProblem = 0;
+    nextProblem();
+}
+function setContent(content){
+    document.body.innerHTML=content;
+}
+function main(){
+    let content = "";
+    content+=`<p><button><img src="./unicorn.png" onclick="startGame('./unicorn.png')"/></button><button><img src="./ghost.png"/ onclick="startGame('./ghost.png')"></button></p>`;
+    setContent(content);
+}
