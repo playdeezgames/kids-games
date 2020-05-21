3c46788dde64c374da9c14d1a179a211def87855
ernestpazera <ernestpazera@gmail.com>
2020-05-21 05:22:50 -0500

    letters

---
+++ public/learning/reading/smallletters/main.js
@@ -0,0 +1,100 @@
+const TOTAL_ROUNDS = 20;
+const table={
+    "A":"a",
+    "B":"b",
+    "C":"c",
+    "D":"d",
+    "E":"e",
+    "F":"f",
+    "G":"g",
+    "H":"h",
+    "I":"i",
+    "J":"j",
+    "K":"k",
+    "L":"l",
+    "M":"m",
+    "N":"n",
+    "O":"o",
+    "P":"p",
+    "Q":"q",
+    "R":"r",
+    "S":"s",
+    "T":"t",
+    "U":"u",
+    "V":"v",
+    "W":"w",
+    "X":"x",
+    "Y":"y",
+    "Z":"z"
+};
+const list=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+let gameState = {};
+function randomRange(minimum, maximum){
+    return Math.floor(Math.random()*(maximum-minimum+1))+minimum;
+}
+function giveAnswer(answer){
+    if(gameState.answerGiven){
+        return;
+    }
+    gameState.answerGiven=true;
+    if(answer==gameState.currentProblem){
+        gameState.roundsCorrect++;
+        document.getElementById("result").innerHTML=`<img src="./confirmed.png"/>`;
+        setTimeout(()=>{
+            nextProblem();
+        },2000);
+    }else{
+        gameState.badGuesses++;
+        document.getElementById("result").innerHTML=`<img src="./cancel.png"/>`;
+        setTimeout(()=>{
+            document.getElementById("result").innerHTML="";
+            gameState.answerGiven=false;
+        },2000);
+    }
+}
+function showProblem(){
+    let content = "";
+    content+=`<p>#${gameState.totalRounds-gameState.roundsLeft} of ${gameState.totalRounds}</p><hr/>`;
+    content+=`<h1>${gameState.currentProblem}</h1><hr/>`;
+    content+="<p>"
+    for(let item of list){
+        content+=`<button onclick="giveAnswer('${item}')"><h3> ${table[item]} </h3></button> `;
+    }
+    content+=`</p><span id="result"></span><p></p>`;
+    setContent(content);
+}
+function gameOver(){
+    let content="";
+    content+="<h1>All done!</h1>";
+    content+=`<p>Rounds: ${gameState.roundsCorrect}</p>`;
+    content+=`<p>Wrong Guesses: ${gameState.badGuesses}</p>`;
+    setContent(content);
+}
+function nextProblem(){
+    if(gameState.roundsLeft<=0){
+        gameOver();
+        return;
+    }
+    let newProblem;
+    do{
+        newProblem = list[randomRange(0,list.length-1)];
+    }while(newProblem==gameState.currentProblem);
+    gameState.currentProblem = newProblem;
+    gameState.answerGiven = false;
+    gameState.roundsLeft--;
+    showProblem();
+}
+function startGame(){
+    gameState.roundsLeft = TOTAL_ROUNDS;
+    gameState.totalRounds = TOTAL_ROUNDS;
+    gameState.roundsCorrect = 0;
+    gameState.badGuesses = 0;
+    gameState.currentProblem = {}
+    nextProblem();
+}
+function setContent(content){
+    document.body.innerHTML=content;
+}
+function main(){
+    startGame();
+}
