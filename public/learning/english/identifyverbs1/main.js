b1b446e7cc49ebbbf4d01061451568f7f238c4e1
ernestpazera <ernestpazera@gmail.com>
2020-05-26 04:43:59 -0500

    added relative pronoun exercise and identify verbs for first graders and added timing to relative pronouns

---
+++ public/learning/english/identifyverbs1/main.js
@@ -0,0 +1,154 @@
+const TOTAL_ROUNDS = 20;
+const list=[];
+const used=[];
+function addToList(text,answer){
+    list.push({
+        text: text,
+        answer: answer
+    });
+}
+addToList("bicycle",false);
+addToList("read",true);
+addToList("book",false);
+addToList("tell",true);
+addToList("story",false);
+addToList("looks",true);
+addToList("funny",false);
+addToList("cat",false);
+addToList("fun",false);
+addToList("plays",true);
+addToList("with",false);
+addToList("child",false);
+addToList("dog",false);
+addToList("runs",true);
+addToList("around",false);
+addToList("the",false);
+addToList("take",true);
+addToList("glass",false);
+addToList("drinks",true);
+addToList("eat",true);
+addToList("food",false);
+addToList("pie",false);
+addToList("pasta",false);
+addToList("cuts",true);
+addToList("boys",false);
+
+addToList("bells",false);
+addToList("ring",true);
+addToList("sounds",false);
+addToList("music",false);
+addToList("walk",true);
+addToList("climb",true);
+addToList("sliding",true);
+addToList("path",false);
+addToList("the",false);
+addToList("leaves",true);
+addToList("falling",true);
+addToList("girl",false);
+addToList("talk",true);
+addToList("friends",false);
+addToList("piano",false);
+addToList("flute",false);
+addToList("rise",true);
+addToList("think",true);
+addToList("table",false);
+addToList("sat",true);
+addToList("onto",false);
+addToList("couch",false);
+addToList("put",true);
+addToList("feet",false);
+addToList("chair",false);
+addToList("mitten",false);
+
+addToList("swim",true);
+addToList("ride",true);
+addToList("broom",false);
+addToList("wish",true);
+addToList("tree",false);
+addToList("work",true);
+addToList("car",false);
+addToList("sleep",true);
+addToList("try",true);
+addToList("pail",false);
+addToList("flower",false);
+addToList("find",true);
+addToList("make",true);
+addToList("bird",false);
+addToList("go",true);
+addToList("sit",true);
+addToList("couch",false);
+
+let gameState = {};
+function randomRange(minimum, maximum){
+    return Math.floor(Math.random()*(maximum-minimum+1))+minimum;
+}
+function giveAnswer(answer){
+    if(gameState.answerGiven){
+        return;
+    }
+    gameState.answerGiven=true;
+    if(answer==gameState.currentProblem.answer){
+        gameState.roundsCorrect++;
+        document.getElementById("result").innerHTML=`<img src="../../assets/confirmed.png"/>`;
+        setTimeout(()=>{
+            nextProblem();
+        },2000);
+    }else{
+        gameState.badGuesses++;
+        document.getElementById("result").innerHTML=`<img src="../../assets/cancel.png"/>`;
+        setTimeout(()=>{
+            document.getElementById("result").innerHTML="";
+            gameState.answerGiven=false;
+        },2000);
+    }
+}
+function showProblem(){
+    let content = "";
+    content+=`<p>#${gameState.totalRounds-gameState.roundsLeft} of ${gameState.totalRounds}</p><hr/>`;
+    content+=`<h1>${gameState.currentProblem.text}</h1><hr/>`;
+    content+="<p>"
+    if(randomRange(0,1)==0){
+        content+=`<button onclick="giveAnswer(true)"><h3> Verb </h3></button> `;
+        content+=`<button onclick="giveAnswer(false)"><h3> <strike>Verb</strike> </h3></button> `;
+    }else{
+        content+=`<button onclick="giveAnswer(false)"><h3> <strike>Verb</strike> </h3></button> `;
+        content+=`<button onclick="giveAnswer(true)"><h3> Verb </h3></button> `;
+    }
+    content+=`</p><span id="result"></span><p></p>`;
+    setContent(content);
+}
+function gameOver(){
+    let content="";
+    content+="<h1>All done!</h1>";
+    content+=`<p>Rounds: ${gameState.roundsCorrect}</p>`;
+    content+=`<p>Wrong Guesses: ${gameState.badGuesses}</p>`;
+    setContent(content);
+}
+function nextProblem(){
+    if(gameState.roundsLeft<=0){
+        gameOver();
+        return;
+    }
+    let newProblem;
+    do{
+        newProblem = list[randomRange(0,list.length-1)];
+    }while(used.find(x=>x==newProblem)!=null);
+    gameState.currentProblem = newProblem;
+    gameState.answerGiven = false;
+    gameState.roundsLeft--;
+    showProblem();
+}
+function startGame(){
+    gameState.roundsLeft = TOTAL_ROUNDS;
+    gameState.totalRounds = TOTAL_ROUNDS;
+    gameState.roundsCorrect = 0;
+    gameState.badGuesses = 0;
+    gameState.currentProblem = {}
+    nextProblem();
+}
+function setContent(content){
+    document.body.innerHTML=content;
+}
+function main(){
+    startGame();
+}
---
+++ public/learning/english/relativepronouns/main.js
@@ -0,0 +1,118 @@
+const TOTAL_ROUNDS = 15;
+const list=[];
+const used=[];
+function addToList(text,answer,choices){
+    list.push({
+        text: text,
+        answer: answer,
+        choices: choices
+    });
+}
+
+addToList("Do you know <u>??????</u> is coming to the party?","who",["who","whom","whose"]);
+addToList("The student to <u>??????</u> I spoke was very kind.","whom",["who","whom","whose"]);
+addToList("You are eating dinner with <u>??????</u>?","whom",["who","whom","whose"]);
+addToList("I have no idea <u>??????</u> notebook this is.","whose",["who","whom","whose"]);
+addToList("I saw a man <u>??????</u> was at least seven feet tall.","who",["who","whom","whose"]);
+addToList("<u>??????</u> is your favorite teacher?","who",["who","whom","whose"]);
+addToList("She doesn’t know <u>??????</u> book was left behind.","whose",["who","whom","whose"]);
+addToList("My mom asked <u>??????</u> made the card for her.","who",["who","whom","whose"]);
+addToList("For <u>??????</u> did you make this birthday cake?","whom",["who","whom","whose"]);
+addToList("<u>??????</u> car is parked in our garage?","whose",["who","whom","whose"]);
+
+addToList("The girl with <u>??????</u> I worked was very quiet.","whom",["who","whom","whose"]);
+addToList("You are talking to <u>??????</u>?","whom",["who","whom","whose"]);
+addToList("Does anyone know <u>??????</u> backpack this is?","whose",["who","whom","whose"]);
+addToList("She talked to a woman <u>??????</u> knows my mom.","who",["who","whom","whose"]);
+addToList("<u>??????</u> was at the park today?","who",["who","whom","whose"]);
+addToList("Darren has no clue <u>??????</u> sock was in his car.","whose",["who","whom","whose"]);
+addToList("My dad wondered <u>??????</u> forgot to lock the door.","who",["who","whom","whose"]);
+addToList("With <u>??????</u> did you ride to school today?","whom",["who","whom","whose"]);
+addToList("<u>??????</u> cat is walking in our yard?","whose",["who","whom","whose"]);
+addToList("Mrs. Smith asked <u>??????</u> had finished the assignment.","who",["who","whom","whose"]);
+
+addToList("You are traveling with <u>??????</u> toChicago?","whom",["who","whom","whose"]);
+addToList("Did you find out <u>??????</u> house we are visiting?","whose",["who","whom","whose"]);
+addToList("The man <u>??????</u> came with us is my uncle.","who",["who","whom","whose"]);
+addToList("<u>??????</u> is that girl with your cousin?","who",["who","whom","whose"]);
+addToList("Sharon doesn’t know <u>??????</u> pencil was on the desk.","whose",["who","whom","whose"]);
+addToList("We asked <u>??????</u> was making dinner tonight.","who",["who","whom","whose"]);
+addToList("For <u>??????</u> did you buy that gift?","whom",["who","whom","whose"]);
+addToList("<u>??????</u> baby is crying all night?","whose",["who","whom","whose"]);
+addToList("My brother wondered <u>??????</u> took his backpack.","who",["who","whom","whose"]);
+addToList("Did you see the boy <u>??????</u> bike was stolen?","whose",["who","whom","whose"]);
+
+list.sort((a,b)=>randomRange(0,1)*2-1);
+
+let gameState = {};
+function randomRange(minimum, maximum){
+    return Math.floor(Math.random()*(maximum-minimum+1))+minimum;
+}
+function giveAnswer(answer){
+    if(gameState.answerGiven){
+        return;
+    }
+    gameState.answerGiven=true;
+    if(answer==gameState.currentProblem.answer){
+        gameState.roundsCorrect++;
+        document.getElementById("result").innerHTML=`<img src="../../assets/confirmed.png"/>`;
+        setTimeout(()=>{
+            nextProblem();
+        },2000);
+    }else{
+        gameState.badGuesses++;
+        document.getElementById("result").innerHTML=`<img src="../../assets/cancel.png"/>`;
+        setTimeout(()=>{
+            document.getElementById("result").innerHTML="";
+            gameState.answerGiven=false;
+        },2000);
+    }
+}
+function showProblem(){
+    let content = "";
+    content+=`<p>#${gameState.totalRounds-gameState.roundsLeft} of ${gameState.totalRounds}</p><hr/>`;
+    content+=`<h1>${gameState.currentProblem.text}</h1><hr/>`;
+    content+="<p>"
+    let choices = gameState.currentProblem.choices.sort((a,b)=>randomRange(0,1)*2-1);
+    for(let choice of choices){
+        content+=`<button onclick="giveAnswer('${choice}')"><h3> ${choice} </h3></button> `;
+
+    }
+    content+=`</p><span id="result"></span><p></p>`;
+    setContent(content);
+}
+function gameOver(){
+    let content="";
+    gameState.endTime = Date.now();
+    content+="<h1>All done!</h1>";
+    content+=`<p>Rounds: ${gameState.roundsCorrect}</p>`;
+    content+=`<p>Wrong Guesses: ${gameState.badGuesses}</p>`;
+    content+=`<p>Time Taken: ${Math.floor((gameState.endTime-gameState.startTime)/1000)}</p>`;
+    setContent(content);
+}
+function nextProblem(){
+    if(gameState.roundsLeft<=0){
+        gameOver();
+        return;
+    }
+    gameState.currentProblem = list.shift();
+    gameState.answerGiven = false;
+    gameState.roundsLeft--;
+    showProblem();
+}
+function startGame(){
+    gameState.roundsLeft = TOTAL_ROUNDS;
+    gameState.totalRounds = TOTAL_ROUNDS;
+    gameState.roundsCorrect = 0;
+    gameState.badGuesses = 0;
+    gameState.currentProblem = {};
+    gameState.startTime = Date.now();
+    nextProblem();
+}
+function setContent(content){
+    document.body.innerHTML=content;
+}
+
+function main(){
+    setContent(`<button onclick="startGame()">Start</button>`);
+}
